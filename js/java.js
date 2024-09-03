e5fc7c07db284fe3b440ce22c2a687f7faf3f78c
Nastya Star <harryisokay@gmail.com>
2024-09-03 15:47:28 +0300

    added java

---
+++ js/java.js
@@ -0,0 +1,73 @@
+
+let a = Number(20);
+alert(a); 
+
+
+
+let Year= ("2007");
+alert( `Год выпуска первого iPhone- ${Year}`); 
+
+
+
+let  Surname= ('Brendon Eich');
+alert(`Создатель языка JavaScript- ${Surname}`);
+
+
+
+
+let a = Number(10);
+let b =Number(2);
+let sum= a+b;
+let difference= a-b;
+let product= a*b;
+let quotient= a/b;
+alert(`Сумма переменных ${sum},
+Разность переменных ${difference},
+Произведение переменных ${product}, 
+Частное переменных ${quotient}.`);
+
+
+
+let a = Number(2);
+let power = a**5;
+alert(power);
+
+
+
+
+let a= Number(9);
+let b= Number(2);
+let remainder= a%b;
+alert(remainder);
+
+
+
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+
+
+
+let age = Number(prompt( "Сколько вам лет?"));
+alert( ` Вам ${age}`);  
+
+
+
+
+const user ={
+    name:"Willy",
+    age:3,
+isAdmin: true};
+
+
+
+let username = prompt("Введите ваше имя:");
+alert(`Привет, ${username}!`);
